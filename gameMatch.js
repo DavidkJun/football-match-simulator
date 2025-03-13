37e79beca502c78f8fec34570084bd9b7289eac5
Davyd Krykota <krikotadavid957@gmail.com>
2025-03-13 19:24:26 +0200

    rewrited some functions with switch and fixed some bugs

--- gameMatch.js
+++ gameMatch.js
@@ -1,5 +1,9 @@
 const {team1,team2,team3,team4} = require('./sport_teams')
-const {firstMathTeams} = require('./main')
+const {firstMatchTeams} = require('./main')
+const {secondMatchTeams} = require('./main')
+const {team1Name,team2Name,team3Name,team4Name} = require("./sport_teams");
+
+let teamNames = [team1Name, team2Name, team3Name,team4Name]
 
 let actionsAray = [
     takeTheBall,
@@ -9,14 +13,57 @@ let actionsAray = [
 
 let isBallTaken = false;
 
-function action(teams) {
-    let randActionId = Math.floor(Math.random() * 3)
-    let randTeamId =   Math.floor(Math.random() * 2);
-    //first match ball
-    if(isBallTaken === false) {
+function getMatchTeam(teamName, match) {
+    switch (match) {
+        case 1:
+            if(firstMatchTeams.includes(teamName)) {
+                for (let team of teamName) {
+                    if (team === team1Name){
+                        return team1;
+                    }else if(team === team2Name) {
+                        return team2;
+                    }else if(team === team3Name){
+                        return team3;
+                    }else {
+                        return team4;
+                    }
+                }
+            }
+        case 2:
+            if(secondMatchTeams.includes(teamName)) {
+                for (let team of teamName) {
+                    if (team === team1Name){
+                        return team1;
+                    }else if(team === team2Name) {
+                        return team2;
+                    }else if(team === team3Name){
+                        return team3;
+                    }else {
+                        return team4;
+                    }
+                }
+            }
+    }
+}
 
-        takeTheBall()
-        isBallTaken = true;
+function action(match) {
+    let randActionId = Math.floor(Math.random() * 3)
+    let randTeamId = Math.floor(Math.random() * 2);
+
+    switch (match) {
+        case 1:
+            if(isBallTaken === false){
+                takeTheBall(getMatchTeam(firstMatchTeams[randTeamId], 1));
+                isBallTaken = true;
+                break
+            }
+
+        case 2:
+            if(isBallTaken === false){
+                takeTheBall(getMatchTeam(secondMatchTeams[randTeamId], 2));
+                isBallTaken = true;
+                break;
+            }
     }
 }
 
@@ -27,8 +74,7 @@ function takeTheBall(team) {
     console.log(`Player ${playersArr[randPlayerId]} takes the ball`)
 }
 
-//takeTheBall(team1)
-action(firstMathTeams)
+action(1)
 
 function hit() {
 
